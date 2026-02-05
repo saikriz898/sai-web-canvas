8f3eaff86eb4c613d5d2a1f9bcfe66320de27418
Sai Krishnan S <saikrishnan.s2024cse@sece.ac.in>
2026-02-05 17:09:05 +0530

    feat: Add reusable components

---
+++ src/components/TypeWriter.jsx
@@ -0,0 +1,36 @@
+const TypeWriter = ({ words }) => {
+    const [index, setIndex] = useState(0);
+    const [subIndex, setSubIndex] = useState(0);
+    const [reverse, setReverse] = useState(false);
+    const [blink, setBlink] = useState(true);
+
+    // Blinking cursor
+    useEffect(() => {
+        const timeout2 = setTimeout(() => {
+            setBlink((prev) => !prev);
+        }, 500);
+        return () => clearTimeout(timeout2);
+    }, [blink]);
+
+    // Typing effect
+    useEffect(() => {
+        if (subIndex === words[index].length + 1 && !reverse) {
+            setReverse(true);
+            return;
+        }
+
+        if (subIndex === 0 && reverse) {
+            setReverse(false);
+            setIndex((prev) => (prev + 1) % words.length);
+            return;
+        }
+
+        const timeout = setTimeout(() => {
+            setSubIndex((prev) => prev + (reverse ? -1 : 1));
+        }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, parseInt(Math.random() * 350)));
+
+        return () => clearTimeout(timeout);
+    }, [subIndex, index, reverse, words]);
+
+    return <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-400">{`${words[index].substring(0, subIndex)}`}</span>;
+};
